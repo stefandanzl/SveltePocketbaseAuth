4f8e166979ee12b97cbcd37e5dab8b027de54c9a
Stefan Danzl <stefan.danzl@live.de>
2023-11-06 17:36:45 +0100

    zwei

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,12 @@
   import LoginScreen from "./LoginScreen.svelte";
   import { currentUser } from "./lib/pocketbase";
   import UserScreen from "./UserScreen.svelte";
+  import Tabs from "./Tabs.svelte";
 </script>
 
 {#if !$currentUser}
   <LoginScreen />
 {:else}
   <UserScreen />
+  <Tabs />
 {/if}
---
+++ src/Tabs.svelte
@@ -0,0 +1,47 @@
+<script>
+    //@ts-nocheck
+    import { Tabs, TabItem, Timeline, TimelineItem, Button } from 'flowbite-svelte';
+    import { ArrowRightOutline } from 'flowbite-svelte-icons';
+  </script>
+  
+  <Tabs>
+    <TabItem open>
+      <span slot="title">Profile</span>
+      <Timeline>
+        <TimelineItem title="Application UI code in Tailwind CSS" date="February 2022">
+          <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
+          <Button color="alternative">
+            Learn more
+            <ArrowRightOutline class="ml-2 w-3 h-3" />
+          </Button>
+        </TimelineItem>
+        <TimelineItem title="Application UI code in Tailwind CSS" date="March 2022">
+          <p class="text-base font-normal text-gray-500 dark:text-gray-400">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
+        </TimelineItem>
+        <TimelineItem title="Application UI code in Tailwind CSS" date="April 2022">
+          <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements built on top of Tailwind CSS.</p>
+        </TimelineItem>
+      </Timeline>
+    </TabItem>
+    <TabItem>
+      <span slot="title">Dashboard</span>
+      <p class="text-sm text-gray-500 dark:text-gray-400">
+        <b>Dashboard:</b>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </TabItem>
+    <TabItem>
+      <span slot="title">Settings</span>
+      <p class="text-sm text-gray-500 dark:text-gray-400">
+        <b>Settings:</b>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </TabItem>
+    <TabItem>
+      <span slot="title">Users</span>
+      <p class="text-sm text-gray-500 dark:text-gray-400">
+        <b>Users:</b>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </TabItem>
+  </Tabs>
